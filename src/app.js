05dfd3477ed7a11270d62f40afb488b5afef60c9
hyunjung lee <hyunjung lee>
2017-06-13 13:26:03 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>This is the react App!</div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
