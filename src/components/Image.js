422720a22aa34425796b5fd96b1d87ec3c635a1e
hyunjung lee <hyunjung lee>
2017-06-13 14:55:28 +0200

    Cloudinary API

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
 
 class Images extends Component {
+
+    uploadFile(files) {
+        console.log('uploadFile: ')
+        const image = files[0]
+        // https://media-service.appspot.com/site/images/7OWDvgtE?crop=96
+        // https://api.cloudinary.com/v1_1/<cloud name>/<resource_type>/upload
+
+        const cloudName= 'denq2af9d'
+        const url = "https://api.cloudinary.com/v1_1/"+cloudName+"/img/upload"
+
+        const timestamp = Date.now()/1000
+        const uploadPreset = 'rnxsz09i'
+
+        const paramsStr = 'timestamp='+timestamp='&upload_preset'+uploadPreset
+        const signature = shq1(paramsStr)
+
+        const params = {
+            'api_key': '728318899993142',
+            'timestamp': timestamp,
+            'upload_preset': uploadPreset,
+            'signature': signature
+        }
+
+
+    }
 
     render() {
         return (
             <div>
                 Images Component
+                <Dropzone onDrop={this.uploadFile.bind(this)}/>
             </div>
         )
     }
